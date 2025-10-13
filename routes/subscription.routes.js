25d184734594ea4900f0726fdf7270c651af7703
Arjun Singh Bhadauriya <arjun22august@gmail.com>
2025-10-13 22:49:00 +0530

    Initial commit

---
+++ routes/subscription.routes.js
@@ -0,0 +1,5 @@
+import { Router } from 'express';
+const subscriptionRouter = Router();
+
+/*This is my own private domicile and I will not be harassed bitch!
+    like if its own thing is not affected by its abnormal properties.*/
